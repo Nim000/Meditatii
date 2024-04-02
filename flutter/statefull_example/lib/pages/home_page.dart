62980490de759e508efb2b2d4642f468e87965f9
baragan30 <baragan30@yahoo.com>
2024-04-02 09:51:05 +0300

    statefull widget

---
+++ flutter/statefull_example/lib/pages/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Center"),
+            Container(
+              width: 200,
+              height: 200,
+              color: color,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  color = Colors.red;
+                });
+              },
+              child: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
