5cb6e7cc2af882667d55edea410b3c26c140c485
Nim000 <mihailchivu8@gmail.com>
2024-06-06 12:01:55 +0300

    {message}

--- flutter/store/lib/pages/cart_page.dart
+++ flutter/store/lib/pages/cart_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:store/model/dummy_data.dart';
+import 'package:store/model/meal.dart';
+import 'package:store/widgets/list_card.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
+  Widget totalPriceWidget(double height, double price) {
+    return Container(
+      height: height,
+      alignment: Alignment.centerRight,
+      child: Text("Total:"),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,30 +20,34 @@ class CartPage extends StatelessWidget {
     const double totalPriceHeight = 100;
     return Scaffold(
       body: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-        int nrOfTiles = 10;
-        double totalHeight = nrOfTiles * tileHeight + totalPriceHeight;
-        return Column(
-          children: [
-            Expanded(
-              child: ListView(
-                children: [
-                  for (int i = 0; i < nrOfTiles; i++)
-                    Container(
-                      height: tileHeight,
-                      width: 100,
-                      color: Colors.amber,
-                    )
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              child: Text("Total:"),
-            ),
-          ],
-        );
-      }),
+        builder: (BuildContext context, BoxConstraints constraints) {
+          List<Meal> meals = DummyData.getSelectedMeals();
+          int nrOfTiles = meals.length;
+          double totalHeight = nrOfTiles * tileHeight + totalPriceHeight;
+          if (constraints.maxHeight < totalHeight)
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    children: [
+                      for (int i = 0; i < nrOfTiles; i++)
+                        ListCard(meal: meals[i], height: tileHeight)
+                    ],
+                  ),
+                ),
+                totalPriceWidget(totalPriceHeight, 100)
+              ],
+            );
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              for (int i = 0; i < nrOfTiles; i++)
+                ListCard(meal: meals[i], height: tileHeight),
+              totalPriceWidget(totalPriceHeight, 100),
+            ],
+          );
+        },
+      ),
     );
   }
 }
---
+++ flutter/store/lib/widgets/list_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:store/model/dummy_data.dart';
+import 'package:store/model/meal.dart';
+
+class ListCard extends StatelessWidget {
+  ListCard({required this.meal, required this.height});
+  Meal meal;
+  double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      color: Color.fromARGB(0, 255, 193, 7),
+      child: ListTile(
+        leading: Container(
+          width: 40,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(DummyData.imagesUrl[0]),
+            ),
+          ),
+        ),
+        title: Text(meal.name),
+        subtitle: Row(
+          children: [
+            Text(
+              '\$${meal.price}',
+              style: TextStyle(
+                  color: Color(0xFF8A3490),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            if (meal.isDiscounted())
+              Text(
+                '\$${meal.originalPrice}',
+                style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
